404424e28a9b5ad3535c6f3669d2681ecc6b785b
Zeeshan Aslam <zesh1984@gmail.com>
2025-11-27 18:20:19 +0500

    updated allowed_hosts for ecs

--- proxy/run.sh
+++ proxy/run.sh
@@ -1,7 +1,8 @@
 #!/bin/sh
 set -e
 
-envsubst < /etc/nginx/default.conf.tpl > /etc/nginx/conf.d/default.conf
+# Replace only the required env variables in the template
+envsubst '$APP_HOST $APP_PORT $LISTEN_PORT' < /etc/nginx/default.conf.tpl > /etc/nginx/conf.d/default.conf
 
-
+# Start Nginx in foreground
 nginx -g "daemon off;"
