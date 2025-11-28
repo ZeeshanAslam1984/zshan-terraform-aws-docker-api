1cefedf1945dae5ce5bb85f2e5e268dfefab195b
Zeeshan Aslam <zesh1984@gmail.com>
2025-11-28 21:28:20 +0500

    updated ecs task to use efs

--- Infra/deploy/ecs.tf
+++ Infra/deploy/ecs.tf
@@ -31,6 +31,12 @@ resource "aws_iam_policy" "task_ssm_policy" {
   policy      = file("./templates/ecs/task-ssm-policy.json")
 }
 
+resource "aws_iam_role_policy_attachment" "task_exec_ssm" {
+  role       = aws_iam_role.task_execution_role.name
+  policy_arn = "arn:aws:iam::aws:policy/AmazonSSMManagedInstanceCore"
+}
+
+
 resource "aws_iam_role_policy_attachment" "task_ssm_policy" {
   role       = aws_iam_role.app_task.name
   policy_arn = aws_iam_policy.task_ssm_policy.arn
